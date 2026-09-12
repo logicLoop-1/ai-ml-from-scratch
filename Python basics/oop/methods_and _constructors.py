b708dba36b9d6ed0de73a1e3b54dfa50da613258
menka hotwani <263845063+logicLoop-1@users.noreply.github.com>
2026-09-12 15:02:00 +0500

    Update methods_and _constructors.py

--- Python basics/oop/methods_and _constructors.py
+++ Python basics/oop/methods_and _constructors.py
@@ -24,3 +24,23 @@ student1.add_grade(88)
 
 print(student1)
 print("Average:", student1.average())
+
+#usage of __eq__
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __eq__(self, other):
+        return (
+            self.title == other.title
+            and self.author == other.author
+            and self.pages == other.pages
+        )
+
+
+book1 = Book("Happy Place", "Emily Henry", 334)
+book2 = Book("Happy Place", "Emily Henry", 334)
+
+print(book1 == book2)
